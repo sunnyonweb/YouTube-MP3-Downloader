ad13dfa72456b90a69a8e3a77c087b06da7c45a5
sunnyonweb <sunnytalukder222@gmail.com>
2026-05-12 00:38:48 +0600

    Set backend API URL to Render

--- frontend/config.js
+++ frontend/config.js
@@ -4,4 +4,4 @@
 // For local development leave this file commented out or unset to use http://127.0.0.1:5000
 // (the frontend defaults to http://127.0.0.1:5000 when window.API_BASE is not set)
 
-// window.API_BASE = 'https://ck444-game.xyz';
+window.API_BASE = 'https://youtube-mp3-downloader-3d6v.onrender.com';
